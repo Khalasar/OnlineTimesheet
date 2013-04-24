7929d25f3203512a3a0c150ddb40208094de3dd3
Andre Stuhrmann <as@neopoly.de>
2013-04-24 12:06:44 +0200

    create Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineTimesheets::Application.config.secret_token = '325b965ebe36ab55e5de1fe8e076d5e127747736ef8ec159da89338212ab3f16b93b2b58fbf87bad936f51f6e2a8dc12b1f58863858a9a0fa1f300cb6e83b5e2'
